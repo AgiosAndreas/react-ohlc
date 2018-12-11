c17d6d03e8b45e503d0931521848e8cc725645a0
Andrei Gaevskii <andrej.gaevskij@gmail.com>
2018-12-11 22:15:30 +0200

    Stub api ready

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,47 @@
-console.log('test');
+import * as express from 'express'
+
+const app = express()
+
+function generateData () {
+
+  const rowsCount = Math.floor(10 * Math.random()) + 1
+
+  const rows = []
+
+  for (let i = 0; i <= rowsCount; i++) {
+    rows.push({
+      o: Math.random(),
+      h: Math.random(),
+      l: Math.random(),
+      c: Math.random()
+    })
+  }
+
+  return {
+    status: 200,
+    ohlc: rows
+  }
+}
+
+app.get(
+  '/:year',
+  (req, res) => {
+    const callback = req.query['callback']
+
+    if (!callback) {
+      res.status(401)
+    }
+
+    res.send(`
+      ${callback}(
+        ${JSON.stringify(generateData())}
+      )
+    `)
+    res.end()
+  }
+)
+
+const port = process.env.PORT || 3001
+
+app.listen(port)
+console.log(`Server started on port: ${port}`)
